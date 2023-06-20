1cff6b0d6d25571f7ba2a5c0f39c41ef2d54d4e0
Momin <md808775@gmail.com>
2023-06-20 12:45:31 +0600

    login page mastering

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,112 @@
-@extends('layouts.app')
+@extends('layouts.auth_app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+@section('auth_content')
+<div class="nk-app-root">
+    <!-- main @s -->
+    <div class="nk-main ">
+        <!-- wrap @s -->
+        <div class="nk-wrap nk-wrap-nosidebar">
+            <!-- content @s -->
+            <div class="nk-content ">
+                <div class="nk-split nk-split-page nk-split-md">
+                    <div class="nk-split-content nk-block-area nk-block-area-column nk-auth-container bg-white">
+                        <div class="absolute-top-right d-lg-none p-3 p-sm-5">
+                            <a href="#" class="toggle btn-white btn btn-icon btn-light" data-target="athPromo"><em class="icon ni ni-info"></em></a>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <div class="nk-block nk-block-middle nk-auth-body">
+                            <div class="brand-logo pb-5">
+                                <a href="html/index.html" class="logo-link">
+                                    <img class="logo-light logo-img logo-img-lg" src="{{ asset('dashboard_asset') }}/images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
+                                    <img class="logo-dark logo-img logo-img-lg" src="{{ asset('dashboard_asset') }}/images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                                </a>
                             </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                            <div class="nk-block-head">
+                                <div class="nk-block-head-content">
+                                    <h5 class="nk-block-title">Sign-In</h5>
                                 </div>
+                            </div><!-- .nk-block-head -->
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <div class="form-label-group">
+                                        <label class="form-label">Enter your E-mail address</label>
+                                    </div>
+                                    <div class="form-control-wrap">
+                                        <input type="text" class="form-control form-control-lg" placeholder="Enter your email address" name="email">
+                                    </div>
+                                </div><!-- .form-group -->
+                                <div class="form-group">
+                                    <div class="form-label-group">
+                                        <label class="form-label">Enter your password</label>
+                                    </div>
+                                    <div class="form-control-wrap">
+                                        <a tabindex="-1" href="#" class="form-icon form-icon-right passcode-switch lg" data-target="password">
+                                            <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                            <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                        </a>
+                                        <input type="password" class="form-control form-control-lg" placeholder="Enter your password" name="password">
+                                    </div>
+                                </div><!-- .form-group -->
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+                                </div>
+                            </form><!-- form -->
+                            
+                        </div><!-- .nk-block -->
+                        <div class="nk-block nk-auth-footer">
+                            <div class="mt-3">
+                                <p>&copy; 2021 DashLite. All Rights Reserved.</p>
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        </div><!-- .nk-block -->
+                    </div><!-- .nk-split-content -->
+                    <div class="nk-split-content nk-split-stretch bg-lighter d-flex toggle-break-lg toggle-slide toggle-slide-right" data-content="athPromo" data-toggle-screen="lg" data-toggle-overlay="true">
+                        <div class="slider-wrap w-100 w-max-550px p-3 p-sm-5 m-auto">
+                            <div class="slider-init" data-slick='{"dots":true, "arrows":false}'>
+                                <div class="slider-item">
+                                    <div class="nk-feature nk-feature-center">
+                                        <div class="nk-feature-img">
+                                            <img class="round" src="{{ asset('dashboard_asset') }}/images/slides/promo-a.png" srcset="./images/slides/promo-a2x.png 2x" alt="">
+                                        </div>
+                                        <div class="nk-feature-content py-4 p-sm-5">
+                                            <h4>Ironman</h4>
+                                            <p>You can start to create your products easily with its user-friendly design & most completed responsive layout.</p>
+                                        </div>
+                                    </div>
+                                </div><!-- .slider-item -->
+                                <div class="slider-item">
+                                    <div class="nk-feature nk-feature-center">
+                                        <div class="nk-feature-img">
+                                            <img class="round" src="{{ asset('dashboard_asset') }}/images/slides/promo-b.png" srcset="./images/slides/promo-b2x.png 2x" alt="">
+                                        </div>
+                                        <div class="nk-feature-content py-4 p-sm-5">
+                                            <h4>Ironman</h4>
+                                            <p>You can start to create your products easily with its user-friendly design & most completed responsive layout.</p>
+                                        </div>
+                                    </div>
+                                </div><!-- .slider-item -->
+                                <div class="slider-item">
+                                    <div class="nk-feature nk-feature-center">
+                                        <div class="nk-feature-img">
+                                            <img class="round" src="{{ asset('dashboard_asset') }}/images/slides/promo-c.png" srcset="./images/slides/promo-c2x.png 2x" alt="">
+                                        </div>
+                                        <div class="nk-feature-content py-4 p-sm-5">
+                                            <h4>Ironman</h4>
+                                            <p>You can start to create your products easily with its user-friendly design & most completed responsive layout.</p>
+                                        </div>
+                                    </div>
+                                </div><!-- .slider-item -->
+                            </div><!-- .slider-init -->
+                            <div class="slider-dots"></div>
+                            <div class="slider-arrows"></div>
+                        </div><!-- .slider-wrap -->
+                    </div><!-- .nk-split-content -->
+                </div><!-- .nk-split -->
             </div>
+            <!-- wrap @e -->
         </div>
+        <!-- content @e -->
     </div>
+    <!-- main @e -->
 </div>
+<!-- app-root @e -->
 @endsection
---
+++ resources/views/layouts/auth_app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <base href="../../../">
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="{{ asset('dashboard_asset') }}/images/favicon.png">
+    <!-- Page Title  -->
+    <title>Login | DashLite Admin Template</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="{{ asset('dashboard_asset') }}/assets/css/dashlite.css?ver=2.9.0">
+    <link id="skin-default" rel="stylesheet" href="{{ asset('dashboard_asset') }}/assets/css/theme.css?ver=2.9.0">
+</head>
+
+<body class="nk-body bg-white npc-general pg-auth">
+
+
+@yield('auth_content')
+
+
+
+
+<!-- JavaScript -->
+<script src="{{ asset('dashboard_asset') }}/assets/js/bundle.js?ver=2.9.0"></script>
+<script src="{{ asset('dashboard_asset') }}/assets/js/scripts.js?ver=2.9.0"></script>
+</body>
+</html>    
